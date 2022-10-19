da1830c719fd98fd6217085f67b26f73339231c9
Kamen Antonov <TPOsmanliev19@codingburgas.bg>
2022-10-19 13:04:04 +0300

    Add class Car and Lamp

---
+++ JavaClasses/src/Lamp.java
@@ -0,0 +1,13 @@
+public class Lamp {
+    String name;
+
+    void setLampName(String name)
+    {
+        this.name=name;
+    }
+    void getLampName()
+    {
+        System.out.println(this.name);
+    }
+
+}
